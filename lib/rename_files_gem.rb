b5c212075366151717c6bdc3d686c4b226e7e7b7
dacaiguoguo <dacaiguoguo@163.com>
2024-04-02 10:09:38 +0800

    init commit

---
+++ lib/rename_files_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rename_files_gem/version"
+
+module RenameFilesGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rename_files_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RenameFilesGem
+  VERSION = "0.1.0"
+end
